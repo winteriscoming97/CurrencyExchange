7765aa5722e0f830301241448d94fb2b64191af8
Justin <justinjackson1198@gmail.com>
2020-05-20 21:05:04 -0700

    more formatting, media links, drop-down from fetch data added

--- src/Footer.js
+++ src/Footer.js
@@ -6,9 +6,9 @@ function Footer () {
 
   return (
     <footer>
-      <i className="fab fa-github"></i>
-      <i className="fab fa-linkedin-in"></i>
-      <i className="fas fa-portrait"></i>
+      <a href="https://github.com/" target="_blank" rel="noopener noreferrer"><i className="fab fa-github"></i><span>Git Hub</span></a>
+      <a href="https://www.linkedin.com/in/justin-j-8a64471a9/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin-in"></i><span>LinkedIn</span></a>
+      <a href="https://zen-roentgen-2ec509.netlify.app/" target="_blank" rel="noopener noreferrer"><i className="fas fa-portrait"></i><span>Portfolio</span></a>
     </footer>
   )
 }
